1817046fd01e50f87d6226af5a1c5cab5b3f24bb
HP-ENVY\Tewelde <teweldemat@gmail.com>
2023-12-13 10:40:07 +0300

    deposit transaction fee bug

--- TgBot/SmartLedger/Dialog/PayDialog.cs
+++ TgBot/SmartLedger/Dialog/PayDialog.cs
@@ -78,7 +78,7 @@ namespace TgBot.SmartLedger.Dialog
                         if (double.TryParse(t, out var d) && d >= 0)
                         {
                             var feeAmount = IntData.toIntMoney(d);
-                            if (feeAmount <= MAX_FEE_PROPORTION * source.Amount)
+                            if (feeAmount <= MAX_FEE_PROPORTION * Math.Abs(source.PayeeFee))
                             {
                                 return Task.FromResult(new ParseResult { Data = feeAmount });
                             }
